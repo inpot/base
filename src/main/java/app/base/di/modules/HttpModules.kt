1badd1d4b8776363fff201c8ef7f6bb9c37d56b7
daniel <winepot2005@gmail.com>
2018-11-15 18:02:33 +0800

    add log for http request

--- src/main/java/app/base/di/modules/HttpModules.kt
+++ src/main/java/app/base/di/modules/HttpModules.kt
@@ -3,6 +3,8 @@ package app.base.di.modules
 import android.content.Context
 import android.content.SharedPreferences
 import android.text.TextUtils
+import android.util.Log
+import app.base.BuildConfig
 import app.base.R
 import app.base.di.scope.PerApplication
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
@@ -16,6 +18,7 @@ import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 /**
  * Created by daniel on 17-10-18.
@@ -29,6 +32,9 @@ object HttpModules {
     fun provideRetrofit(moshi: Moshi, context: Context, preference:SharedPreferences): Retrofit {
         val baseUrl = context.getString(R.string.api_host)
         val okHttpBuilder= OkHttpClient.Builder()
+        okHttpBuilder.connectTimeout(10, TimeUnit.SECONDS)
+        okHttpBuilder.readTimeout(10, TimeUnit.SECONDS)
+        okHttpBuilder.writeTimeout(10,TimeUnit.SECONDS)
         okHttpBuilder.addInterceptor {
             val original = it.request();
             val token = preference.getString("token",null)
@@ -37,8 +43,11 @@ object HttpModules {
                 requestBuilder.addHeader("token", "value1")
             }
             val request = requestBuilder.build();
-            it .proceed(request);
-
+            val response = it .proceed(request);
+            if(BuildConfig.DEBUG){
+                Log.i("http"," ${original.method()} : ${original.url()} + ${original.headers()}")
+            }
+            response
         }
         val builder = Retrofit.Builder().baseUrl(baseUrl)
                 .client(okHttpBuilder.build())
