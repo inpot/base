9701c08c876e34dfe1d57b75f4bafc37ae012909
daniel <winepot2005@gmail.com>
2018-11-19 21:16:53 +0800

    remove unused code

--- src/main/java/app/base/di/modules/HttpModules.kt
+++ src/main/java/app/base/di/modules/HttpModules.kt
@@ -12,7 +12,6 @@ import com.squareup.moshi.Moshi
 import dagger.Module
 import dagger.Provides
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.moshi.MoshiConverterFactory
