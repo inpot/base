11c0d62d5ae83134fc52d06f401ca4086ec17102
daniel <winepot2005@gmail.com>
2018-11-26 15:01:06 +0800

    remove empty constructor

--- src/main/java/app/base/mvvm/vm/BaseVM.kt
+++ src/main/java/app/base/mvvm/vm/BaseVM.kt
@@ -7,22 +7,4 @@ import app.base.mvvm.view.IView
 /**
  * Created by daniel on 17-10-19.
  */
-abstract class BaseVM<Rep : IRepository, V: IView> (){
-    lateinit var repository:Rep
-    lateinit var view:V
-    constructor(repository:Rep,view:V) : this() {
-        this.repository = repository
-        this.view = view
-    }
-
-    open fun isInitialized() :Boolean{
-        val result = ::view.isInitialized && ::repository.isInitialized
-        return result
-    }
-
-    open fun initialize(repository:Rep,view:V){
-        this.repository = repository
-        this.view = view
-    }
-
-}
+abstract class BaseVM<Rep : IRepository, V: IView> (val repository:Rep,val view:V)
