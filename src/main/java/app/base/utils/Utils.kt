a8b8065c276adac69fe2ebae65ceb0f4d21f0aeb
daniel <winepot2005@gmail.com>
2018-12-13 10:25:53 +0800

    add string extension func

---
+++ src/main/java/app/base/utils/Utils.kt
@@ -0,0 +1,37 @@
+package app.base.utils
+
+import android.text.TextUtils
+import android.util.Log
+import java.math.BigDecimal
+import java.util.regex.Pattern
+
+fun String?.toRMB():String{
+    var result = "0.00"
+    if(this != null && !TextUtils.isEmpty(this)){
+        try {
+            val bd = BigDecimal(this)
+            result = bd.setScale(2,BigDecimal.ROUND_HALF_UP).toString()
+        }catch (e:NumberFormatException){
+            e.printStackTrace()
+            Log.e("test","toRMB ${this} is not a number")
+        }
+    }
+    return result
+}
+fun  String?.subZeroAndDot():String {
+    return if(this!=null){
+        val bd = BigDecimal(this)
+        bd.stripTrailingZeros().toPlainString()
+    }else{
+        "0"
+    }
+}
+
+fun String?.isNumberic():Boolean{
+    return if(this == null || TextUtils.isEmpty(this)){
+        false
+    }else{
+        val pattern = Pattern.compile("^-?[0-9]+.?[0-9]*\$")
+        pattern.matcher(this).find()
+    }
+}
